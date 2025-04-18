d0530d27e099a5fda74cc9d8544e81f2e783d4af
iziyeon <dlfvkfclfrn@gmail.com>
2025-04-19 00:43:52 +0900

    feat: CommunityHeader 컴포넌트 구조 생성 및 연결

---
+++ src/components/pages/community/CommunityHeader.tsx
@@ -0,0 +1,3 @@
+export default function CommunityHeader() {
+  return <div>Community Header</div>;
+}
--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,8 +1,10 @@
 import CommunityPostGrid from "../components/pages/community/CommunityPostGrid";
+import CommunityHeader from "../components/pages/community/CommunityHeader";
 
 export default function Community() {
   return (
     <div className="max-w-7xl mx-auto px-4 py-10">
+      <CommunityHeader />
       <CommunityPostGrid />
     </div>
   );
